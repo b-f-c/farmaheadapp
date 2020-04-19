dc111b21da97025eedc5d19f12dcbbfe3cbe96b4
Tyler Suchan <tylersuc@buffalo.edu>
2020-04-18 23:32:53 -0400

    Further landing page improvements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Selection } from './Components/Selection.tsx';
+import Selection from './Components/Selection';
 import logo from './logo.svg';
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <h1>Farm Ahead</h1>
+    <div style={{ position: 'absolute', zIndex: 2 }}>
+      <h1 style={{ textAlign: 'center' }}>Farm Ahead</h1>
       <img src={logo} className="App-logo" alt="logo" />
-    </header>
-    <Selection />
+      <Selection />
+    </div>
+    <div className="App-background" />
   </div>
 );
 
