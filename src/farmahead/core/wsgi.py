90c8ca4c49358192a1eee4d15e2f8e822bc1620a
Brett Cropp <cropp@outlook.com>
2020-04-18 21:10:22 -0400

    changes for deployment

--- src/farmahead/core/wsgi.py
+++ src/farmahead/core/wsgi.py
@@ -2,4 +2,4 @@
 # -*- coding: utf-8 -*-
 import os
 from farmahead.core.app import create_app
-application = create_app(config_cls=os.getenv('FLASK_CONFIG_CLASS', 'production'))
+application = create_app(config_cls=os.getenv('FLASK_CONFIG_CLASS', 'Production'))
