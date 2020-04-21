d3f78c983064f6556a3e86b2628e8d97ff707425
Brett Cropp <cropp@outlook.com>
2020-04-21 08:47:44 -0400

    added routes for produce, added reqparsing

--- src/farmahead/resources/produce.py
+++ src/farmahead/resources/produce.py
@@ -2,37 +2,37 @@ import logging; log=logging.getLogger(__name__)
 from flask import request
 from flask_restful import Resource
 
-from farmahead.models import db, ItemModel, ItemSchema
+from farmahead.models import db, ProduceModel, ProduceSchema
 from farmahead.utils import reply_success, reply_error, reply_missing
 
-schema = ItemSchema()             # dict
-schemas = ItemSchema(many=True)   # list
+schema = ProduceSchema()             # dict
+schemas = ProduceSchema(many=True)   # list
 
 
-class ItemResource(Resource):
+class ProduceResource(Resource):
     """
-    TABLE:  items
-    MODEL:  models.item.ItemModel
-    SCHEMA: models.item.ItemSchema
+    TABLE:  produce
+    MODEL:  models.produce.ProduceModel
+    SCHEMA: models.produce.ProduceSchema
 
-    /api/item
-    /api/item?id=1
+    /api/produce
+    /api/produce?id=1
     """
 
     @staticmethod
     def get_one(_id):
-        log.debug(f'Querying ItemModel where id={_id}')
-        res = db.session.query(ItemModel).filter_by(id=_id).first()
+        log.debug(f'Querying ProduceModel where id={_id}')
+        res = db.session.query(ProduceModel).filter_by(id=_id).first()
         return res
 
     @staticmethod
     def get_all():
-        log.debug(f'Querying ItemModel for all rows')
-        res = db.session.query(ItemModel).all()
+        log.debug(f'Querying ProduceModel for all rows')
+        res = db.session.query(ProduceModel).all()
         return res
 
     def get(self):
-        # Retrieve existing item(s)
+        # Retrieve existing items(s)
         _id = request.args.get('id')
         if not _id:
             res = self.get_all()
