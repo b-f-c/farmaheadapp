cd0de32764104294ca3b2f2d148079b75434cffd
Tyler Suchan <tylersuc@buffalo.edu>
2020-04-23 01:11:54 -0400

    This took ridiculously longer than it needed to. I couldn't figure out a way to set the color of the button that is selected (probably due to sleep deprivation, I'll fix it elsewhere)

--- client/src/components/nav-header/NavHeader.jsx
+++ client/src/components/nav-header/NavHeader.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 
 import FlexBox from '../custom/FlexBox'
+import { Button } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 import { IoIosLeaf } from 'react-icons/io'
 
 const NavHeader = styled.div`
   width: 100%;
-  height: 62px;
+  max-height: 62px;
 
   display: flex;
   justify-content: start;
   align-items: center;
+
+  z-index: 100;
 
   box-shadow: 0px 2px 4px 1px rgba(0, 0, 0, 0.2);
   background-color: #ffffff;
@@ -30,6 +34,22 @@ export default () => {
         <FlexBox style={{ fontSize: '1.5em', paddingLeft: '4px' }}>
           <b>Farm</b>Ahead
         </FlexBox>
+      </FlexBox>
+      <FlexBox
+        justify="center"
+        align="center"
+        direction="row"
+        style={{ paddingLeft: '12px', height: '95%' }}
+      >
+        <LinkContainer to="market">
+          <Button className='Nav-buttons' variant='outline-success'>Markets</Button>
+        </LinkContainer>
+        <LinkContainer to="vendor">
+          <Button className='Nav-buttons' variant='outline-success'>Vendors</Button>
+        </LinkContainer>
+        <LinkContainer to="produce">
+          <Button className='Nav-buttons' variant='outline-success'>Produce</Button>
+        </LinkContainer>
       </FlexBox>
     </NavHeader>
   )
